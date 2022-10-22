a3a30b3ab40c4a83c2f141ff0ffddbd195f674ce
Littlemimss <mariasyrakova@gmail.com>
2022-10-22 11:45:02 +0300

    Edit Build and Run

--- build_and_run_image.bat
+++ build_and_run_image.bat
@@ -1,2 +1,8 @@
+npm install --save-dev web-vitals
+npm install react-scripts
+npm i react
+npm i react-dom
+npm install @ui5/webcomponents-react @ui5/webcomponents @ui5/webcomponents-fiori
+
 cd my-app
 npm start
