e74ac27a51951b8a642776957ce6ad53ff6a965b
Maria Syrakova <111756931+Littlemimss@users.noreply.github.com>
2022-10-22 18:13:01 +0300

    Delete build_and_run_image.bat

--- build_and_run_image.bat
+++
@@ -1,8 +0,0 @@
-npm install --save-dev web-vitals
-npm install react-scripts
-npm i react
-npm i react-dom
-npm install @ui5/webcomponents-react @ui5/webcomponents @ui5/webcomponents-fiori
-
-cd my-app
-npm start
